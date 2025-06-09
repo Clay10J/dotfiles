341e7aef6de34b0de1e50d676d15eb1095bb6a0d
Clay <22086754+Clay10J@users.noreply.github.com>
2025-06-09 16:00:46 -0500

    First steps

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+# fetch the pre-read hook from your GitHub dotfiles repo
+mkdir -p ~/.local/share/chezmoi
+curl -fsSL \
+  https://raw.githubusercontent.com/Clay10J/dotfiles/main/.install-password-manager.sh \
+  -o ~/.local/share/chezmoi/.install-password-manager.sh
+chmod +x ~/.local/share/chezmoi/.install-password-manager.sh
+
+# now run chezmoi as usual
+chezmoi init --apply --password-manager=1password \
+  git@github.com:Clay10J/dotfiles.git
